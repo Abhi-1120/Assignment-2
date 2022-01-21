aa9b039f67f2c2abe964075bae1dedc8263516c3
Abhi <agbhalani11@gmail.com>
2022-01-21 20:56:54 +0530

    Third Commit

--- main.py
+++ main.py
@@ -1,56 +1,60 @@
-# importing the requests library
+# importing libraries
+import os
 import requests
-import json
 import csv
 
-# api-endpoint
-url = 'https://api.github.com/search/repositories?q=is:public+language:Python+forks:>=200'
 
-# sending get request and saving the response as response object
-response = requests.get(url).json()
-
-# fetching all data to data.json file
-with open('data.json', 'w') as file:
-    json.dump(response, file)
-
-# read all data from data.json file
-with open('data.json', 'r') as file:
-    data = json.load(file)
-
-# taking csv files as variables
-output_csv = "output.csv"
-stargazers_count = "stargazers_count.csv"
+def get_date():
+    # api_endpoint
+    url = 'https://api.github.com/search/repositories?q=is:public+language:Python+forks:>=200'
+    # sending get request and saving the response as data object
+    response = requests.get(url).json()
+    return response
 
 
-# Creating methods for writing the data to csv file
-def csv_output():
-    with open(output_csv, 'w', encoding="utf-8") as file:
-        csv_file = csv.writer(file)
-        header = csv_file.writerow(
-            ['language'.upper(), 'description'.upper(), 'html_url'.upper(), 'watchers_count'.upper(),
-             'stargazers_count'.upper(), 'forks_count'.upper()])
+def parse_data(data):
+    # Initializing list for csv file
+    normal_list = []
+    stargazers_list = []
 
-        for item in data['items']:
-            csv_file.writerow(
-                [item['language'], item['description'], item['html_url'], item['watchers_count'],
-                 item['stargazers_count'],
-                 item['forks_count']])
+    # Loop for appending data using filter for both csv file
+    for item in data['items']:
+        csv_data = [item['language'], item['description'], item['html_url'], item['watchers_count'],
+                    item['stargazers_count'], item['forks_count']]
+        normal_list.append(csv_data)
 
-
-# Creating methods for writing limited data to csv file
-def stargazers():
-    with open(stargazers_count, 'w', encoding="utf-8") as file:
-        stargazers_file = csv.writer(file)
-        stargazers_file.writerow(
-            ['language'.upper(), 'description'.upper(), 'html_url'.upper(), 'watchers_count'.upper(),
-             'stargazers_count'.upper(), 'forks_count'.upper()])
-        for item in data['items']:
-            if item['stargazers_count'] > 2000:
-                # Code here will only run if the condition is successful
-                stargazers_file.writerow(
-                    [item['language'], item['description'], item['html_url'], item['watchers_count'],
-                     item['stargazers_count'], item['forks_count']])
+        if item['stargazers_count'] > 2000:
+            stargazers_list.append(csv_data)
+    return normal_list, stargazers_list
 
 
-csv_output()  # function calling
-stargazers()  # function calling
+def write_data_to_csv_file(normal_list, stargazers_list):
+    # header of the csv file
+    header = ['LANGUAGES', 'DESCRIPTION', 'HTML_URL', 'WATCHERS_COUNT', 'STARGAZERS_COUNT', 'FORKS_COUNT']
+
+    # taking csv files as variables
+    normal_csv = "normal_csv"
+    stargazers_csv = "stargazers_csv"
+
+    if os.path.exists(normal_csv):
+        write_csv(normal_csv, normal_list, 'a')
+        write_csv(stargazers_csv, stargazers_list, 'a')
+    else:
+        normal_list.insert(0, header)
+        stargazers_list.insert(0, header)
+
+        write_csv(normal_csv, normal_list)
+        write_csv(stargazers_csv, stargazers_list)
+
+
+# write csv file
+def write_csv(filename, csv_list, action='w'):
+    with open(filename, action, newline="", encoding="utf-8") as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerows(csv_list)
+
+
+# Calling Methods
+data = get_date()
+normal_list, stargazers_list = parse_data(data)
+write_data_to_csv_file(normal_list, stargazers_list)
